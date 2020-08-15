8a077727ea693786774f0d821e36f7ff076930d6
SHAKEEL <SHAKEEL@SHAKEEL-VAIO>
2020-08-15 10:04:59 +0530

    Commting...

---
+++ microsoft/src/main/java/Edge/microdost/App.java
@@ -0,0 +1,13 @@
+package Edge.microdost;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
